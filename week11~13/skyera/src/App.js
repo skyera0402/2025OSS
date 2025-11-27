9b02afa458e0e8895054e905fcfa24194103d771
skyera <162437996+skyera0402@users.noreply.github.com>
2025-11-27 10:26:09 +0900

    router 실습

--- week11~13/skyera/src/App.js
+++ week11~13/skyera/src/App.js
@@ -1,34 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import Time from './Time.js'; 
-import CountEffect from './CountEffect';
-import RefTest from './RefTest';
+//import Time from './Time.js'; 
+//import CountEffect from './CountEffect';
+//import RefTest from './RefTest';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'; 
+import About from './pages/About'; 
 
 function App() {
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          <Time />
-          <CountEffect/>
-          <RefTest/>
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
-      </header>
-    </div>
+    <Router> 
+    <Routes> 
+      <Route path="/" element={<Home />} /> 
+      <Route path="/about" element={<About />} /> 
+      </Routes> 
+    </Router> 
   );
+    
 }
 
 
 export default App;
+
+
+/* <div className="App">
+<header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+
+<p>
+  <Time />
+  <CountEffect/>
+  <RefTest/>
+</p>
+
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+
+</a>
+</header>
+</div>
+*/
